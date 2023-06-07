57449eefbc68eaca97dccbf11592e87feee21709
elyugf <130266867+elyugf@users.noreply.github.com>
2023-06-07 07:37:45 +0700

    Add files via upload

---
+++ predictive system.py
@@ -0,0 +1,21 @@
+import numpy  as pd
+import pickle
+
+#loading the saved ,odel
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+#changing the input data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+#reshape the array as we are predicting for one intance
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+    return('The person is not diabetic')
+else:
+    return('The person is diabetic')
